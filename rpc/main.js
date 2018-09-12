efbf608975f3840a84cc4520c7870984e4aa810a
Sg <shogo1104@gmail.com>
2018-09-12 15:24:00 +0900

    add RPC interface

--- rpc/main.js
+++ rpc/main.js
@@ -2,14 +2,29 @@ var jayson = require('jayson');
 const childChain = require('../childchain')
 // create a server
 var server = jayson.server({
-  tx: function(args, cb) {
+  // These RPC Method must align with ETH
+  eth_sendRawTransaction: (args, cb) => {
     console.log(args)
     childChain.apiTx(args[0]).then((result) => {
       cb(null, result);
     }).catch((err) => {
       cb(err);
     })
-  }
+  },
+  eth_blockNumber: (args, cb) => {
+    // Get latest block for descending manner
+    // https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_blocknumber
+  },
+  eth_getBlockTransactionCountByNumber: (args, cb) => {
+    // Get block info for them
+    // https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_getblocktransactioncountbynumber
+  },
+  eth_getBalance: (args, cb) => {
+
+  },
+  eth_getFilterLogs: (args, cb) => {
+
+  },
 });
 
 server.http().listen(process.env.PORT || 3000);
