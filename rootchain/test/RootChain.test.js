9ed4e2a7e3239048a3a1eb4fea9adc6422e8e733
SHUHEI HIYA <syuhei176@gmail.com>
2018-09-14 17:52:54 +0900

    Add startExit spec

--- rootchain/test/RootChain.test.js
+++ rootchain/test/RootChain.test.js
@@ -1,4 +1,7 @@
 const RootChain = artifacts.require('RootChain');
+const Block = require('../../childchain/lib/block');
+const Transaction = require('../../childchain/lib/tx');
+const { PlasmaStateContract } = require('../../vm');
 
 contract('RootChain', function ([user, owner, recipient, anotherAccount]) {
 
@@ -20,5 +23,44 @@ contract('RootChain', function ([user, owner, recipient, anotherAccount]) {
       assert.equal(result.logs[0].event, 'Deposit');
     });
   });
+
+  describe('startExit', function () {
+    const chain = owner;
+    const utxoPos = 1000000000;
+    let sigs = "00";
+
+
+    it('should startExit', async function () {
+      const depositResult = await this.rootChain.deposit(owner, {value: 1});
+      const block = new Block();
+      const tx = new Transaction();
+      const tx2 = new Transaction();
+      const contract = new PlasmaStateContract('C', 'seed', new Buffer('00', 'hex'), []);
+      contract.exitor = user;
+      const encoded = contract.snapshot();
+      const snapshot = encoded[0];
+      tx.outputs.push(encoded[1]);
+      let txBytes = tx.getBytes();
+      const txBytes2 = tx2.getBytes();
+      block.appendTx(tx);
+      block.appendTx(tx2);
+      let proof = block.createTxProof(tx);
+      const submitResult = await this.rootChain.submitBlock(
+        chain,
+        block.merkleHash(),
+        {from: owner});
+      const startExitResult = await this.rootChain.startExit(
+        chain,
+        utxoPos,
+        txBytes.toString('hex'),
+        snapshot.toString('hex'),
+        proof,
+        sigs,
+        {from: user});
+      assert.equal(startExitResult.logs[0].event, 'ExitStarted');
+    });
+  });
+
+  
   
 });
