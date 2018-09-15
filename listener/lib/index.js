097132175f7e0c1afe8d40f4fa8dbe3c0fcb1044
Sg <shogo1104@gmail.com>
2018-09-15 10:11:57 +0900

    dotenv added

--- listener/lib/index.js
+++ listener/lib/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ROOTCHAIN_ENDPOINT));
 const RootChainAbi = require('../assets/RootChain.json').abi;
