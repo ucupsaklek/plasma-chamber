1f92d52e7594a9a59636034f0d1eb4031da3d39d
SHUHEI HIYA <syuhei176@gmail.com>
2018-09-21 13:51:13 +0900

    Fix deposit

--- childchain/lib/chain.js
+++ childchain/lib/chain.js
@@ -2,7 +2,11 @@ const Block = require('./block');
 const { VMHash } = require('../../vm/lib/operations/crypto');
 const { assembleSource, PlasmaStateContract, PlasmaStateValue } = require('../../vm');
 const Snapshot = require('./state/snapshot');
-const { Transaction } = require('./tx');
+const {
+  Asset,
+  Transaction,
+  TransactionOutput
+} = require('./tx');
 const ChainEvent = require('./chainevent');
 
 class Chain {
@@ -50,17 +54,18 @@ class Chain {
   }
   
   createDepositTx(depositor, amount, depositBlock) {
-    const prog = assembleSource('[put [txid 1 roll get 0 checksig verify] yield]');
-    const stack = [
-      new PlasmaStateValue(amount, new Buffer('d073785d7dffc98c69ef62bbc6c8efde78a3286a848f570f8028695048a8f62d', 'hex', 'anchor')).encode(),
-      depositor
-    ];
-    const contract = new PlasmaStateContract('C', 'contractseed', prog, stack);
-    const encoded = contract.snapshot();
-    const id = VMHash("SnapshotID", encoded[0]);
-    this.snapshot.insertId(id);
-    const depositTx = new Transaction();
-    depositTx.outputs.push(id);
+    const wethTokenAddress = new Buffer("0000000000000000000000000000000000000000", 'hex');
+    const output = new TransactionOutput(
+      [depositor],
+      new Asset(wethTokenAddress, amount)
+    );
+    const depositTx = new Transaction(
+      0,        // label
+      [],       // args
+      0,        // nonce,
+      [],       // inputs
+      [output]  // outputs
+    );
     return depositTx;
   }
   
