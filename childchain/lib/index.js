2a9caaa497685a08229b0c94c36957a46cff9b7a
SHUHEI HIYA <syuhei176@gmail.com>
2018-09-24 15:37:45 +0900

    Fix childchain export classes

--- childchain/lib/index.js
+++ childchain/lib/index.js
@@ -1,15 +1,21 @@
 const Block = require('./block');
 const Chain = require('./chain');
-const Transaction = require('./tx');
+const {
+  Asset,
+  Transaction,
+  TransactionOutput
+} = require('./tx');
 const { apiTx } = require('./api');
 const ChainManager = require("./chain_manager");
 const chainManager = new ChainManager();
 const MongoDown = require('./db/mongodown');
 
 module.exports = {
+  Asset: Asset,
   Block: Block,
   Chain: Chain,
   Transaction: Transaction,
+  TransactionOutput: TransactionOutput,
   apiTx: apiTx,
   run: chainManager.start,
   MongoDown: MongoDown
