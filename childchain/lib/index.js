6eb412922e75edf522521a150e03c7f619805914
SHUHEI HIYA <syuhei176@gmail.com>
2018-09-24 15:55:57 +0900

    Bump 0.0.4

--- childchain/lib/index.js
+++ childchain/lib/index.js
@@ -5,7 +5,6 @@ const {
   Transaction,
   TransactionOutput
 } = require('./tx');
-const { apiTx } = require('./api');
 const ChainManager = require("./chain_manager");
 const chainManager = new ChainManager();
 const MongoDown = require('./db/mongodown');
@@ -16,7 +15,6 @@ module.exports = {
   Chain: Chain,
   Transaction: Transaction,
   TransactionOutput: TransactionOutput,
-  apiTx: apiTx,
   run: chainManager.start,
   MongoDown: MongoDown
 }
