05dca5d75fa7180e1fdd1cd618647eef925d96a6
SHUHEI HIYA <syuhei176@gmail.com>
2018-10-01 17:52:26 +0900

    Update RootChain.test.js

--- rootchain/test/RootChain.test.js
+++ rootchain/test/RootChain.test.js
@@ -13,18 +13,35 @@ const {
   increaseTime
 } = require('openzeppelin-solidity/test/helpers/increaseTime');
 
-contract('RootChain', function ([user, owner, recipient, anotherAccount]) {
+contract('RootChain', function ([user, owner, recipient, user4, user5]) {
 
-  const privKey1 = new Buffer('e88e7cda6f7fae195d0dcda7ccb8d733b8e6bb9bd0bc4845e1093369b5dc2257', 'hex')
-  const privKey2 = new Buffer('855364a82b6d1405211d4b47926f4aa9fa55175ab2deaf2774e28c2881189cff', 'hex')
+  const privKey1 = new Buffer('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', 'hex')
+  const privKey2 = new Buffer('ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f', 'hex')
+  const privKey3 = new Buffer('0dbbe8e4ae425a6d2687f1a7e3ba17bc98c673636790f1b8ad91193c05875ef1', 'hex')
+  const privKey4 = new Buffer('c88b703fb08cbea894b6aeff5a544fb92e78a18e19814cd85da83b71f772aa6c', 'hex')
+  const privKey5 = new Buffer('388c684f0ba1ef5017716adb5d21a053ea8e90277d0868337519f97bede61418', 'hex')
   const testAddress1 = utils.privateToAddress(privKey1);
   const testAddress2 = utils.privateToAddress(privKey2);
+  const testAddress3 = utils.privateToAddress(privKey3);
+  const testAddress4 = utils.privateToAddress(privKey4);
+  const testAddress5 = utils.privateToAddress(privKey5);
   const zeroAddress = new Buffer("0000000000000000000000000000000000000000", 'hex');
 
   beforeEach(async function () {
     this.rootChain = await RootChain.new();
     this.chain = await this.rootChain.addChain({from: owner});
   });
+
+  describe('check', function () {
+    it('should same address', async function () {
+      assert.equal(utils.bufferToHex(testAddress1), user);
+      assert.equal(utils.bufferToHex(testAddress2), owner);
+      assert.equal(utils.bufferToHex(testAddress3), recipient);
+      assert.equal(utils.bufferToHex(testAddress4), user4);
+      assert.equal(utils.bufferToHex(testAddress5), user5);
+    });
+  });
+
 
   describe('addChain', function () {
     it('should add chain', async function () {
@@ -111,8 +128,8 @@ contract('RootChain', function ([user, owner, recipient, anotherAccount]) {
     const utxoPos = blockNumber * 1000000000;
     const blockNumber2 = 1000 * 2;
     const utxoPos2 = blockNumber2 * 1000000000;
-    const tx11 = createTx(testAddress1, recipient, 2, 0, 0);
-    const tx12 = createTx(testAddress1, user, 2, 0, 0);
+    const tx11 = createTx(testAddress1, testAddress3, 2, 0, 0);
+    const tx12 = createTx(testAddress1, testAddress4, 2, 0, 0);
     const sign1 = tx11.sign(privKey1);
     tx11.sigs.push(sign1);
     const sign2 = tx12.sign(privKey1);
@@ -122,9 +139,9 @@ contract('RootChain', function ([user, owner, recipient, anotherAccount]) {
     block1.appendTx(tx12);
     const txindex = block1.getTxIndex(tx11);
 
-    const tx21 = createTx(testAddress2, testAddress1, 2, utxoPos, txindex);
-    const tx22 = createTx(testAddress1, user, 2, 0, 0);
-    const sign21 = tx21.sign(privKey2);
+    const tx21 = createTx(testAddress3, testAddress1, 2, utxoPos, txindex);
+    const tx22 = createTx(testAddress1, testAddress4, 2, 0, 0);
+    const sign21 = tx21.sign(privKey3);
     tx21.sigs.push(sign21);
     const sign22 = tx22.sign(privKey1);
     tx22.sigs.push(sign22);
@@ -174,14 +191,14 @@ contract('RootChain', function ([user, owner, recipient, anotherAccount]) {
         utils.bufferToHex(tx12.getBytes()),
         utils.bufferToHex(proof),
         utils.bufferToHex(sign2),
-        {from: user, gasLimit: 100000});
+        {from: user4, gasLimit: 100000});
       assert(result.hasOwnProperty('receipt'));
       const getExitResult1 = await this.rootChain.getExit(
         owner,
         utxoPos + txindex * 10000 + 0,
         {from: user, gasLimit: 100000});
 
-      assert.equal(getExitResult1[0], user);
+      assert.equal(getExitResult1[0], user4);
       assert.equal(getExitResult1[3].toNumber(), 2);
     });
 
@@ -209,21 +226,30 @@ contract('RootChain', function ([user, owner, recipient, anotherAccount]) {
     });
 
     it('should challengeExit', async function () {
+
+      const submitBlockResult = await this.rootChain.submitBlock(
+        owner,
+        utils.bufferToHex(block2.merkleHash()),
+        {from: owner, gasLimit: 100000});
+
+      assert.equal(submitBlockResult.logs[0].event, 'BlockSubmitted');
+
       const proof21 = block2.createTxProof(tx21);
 
-      try {
-        await this.rootChain.challengeExit(
-          owner,
-          utxoPos2 + block2.getTxIndex(tx21) * 10000,
-          0,
-          utils.bufferToHex(tx21.getBytes()),
-          utils.bufferToHex(proof21),
-          utils.bufferToHex(sign21),
-          utils.bufferToHex(sign21),
-          {from: recipient, gasLimit: 100000});
-      } catch(e) {
-        assert(e.message.indexOf('revert') >= 0);
-      }
+      await this.rootChain.challengeExit(
+        owner,
+        utxoPos2 + block2.getTxIndex(tx21) * 10000,
+        0,
+        utils.bufferToHex(tx21.getBytes()),
+        utils.bufferToHex(proof21),
+        utils.bufferToHex(sign21),
+        utils.bufferToHex(sign21),
+        {from: recipient, gasLimit: 100000});
+      const getExitResultAfter = await this.rootChain.getExit(
+        owner,
+        utxoPos + txindex * 10000 + 0,
+        {from: user, gasLimit: 100000});
+      assert.equal(getExitResultAfter[0], '0x0000000000000000000000000000000000000000');
 
     });
 
