313fc180922ac0adea3cfbf67b9b9f2bb80071e2
SHUHEI HIYA <syuhei176@gmail.com>
2018-10-28 23:25:58 +0900

    Add SparseMerkleTree in child chain

---
+++ childchain/test/smt_test.js
@@ -0,0 +1,47 @@
+const assert = require('assert');
+const SparseMerkleTree = require('../lib/smt');
+const utils = require('ethereumjs-util');
+
+describe('SparceMerkleTree', function() {
+
+  describe('verify', function() {
+
+    const zeroHash = utils.sha3(0);
+
+    it('normal', function() {
+      const leaf = Array.from(Array(Math.pow(2, 16)), () => null);
+      leaf[0] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000001', 'hex');
+      leaf[1] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000002', 'hex');
+      leaf[2] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000003', 'hex');
+      leaf[3] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000004', 'hex');
+      leaf[4] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000005', 'hex');
+      leaf[5] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000006', 'hex');
+      const unknownLeaf = new Buffer('111187814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000006', 'hex');
+      const tree = new SparseMerkleTree(16, leaf);
+      const root = tree.root();
+      const proof = tree.proof(2);
+      assert.equal(tree.root().toString('hex'), '90fa290e1e949c38f8ba0a2ea3a784f6b571c09f5641c240fbe69b1b5c2e03b4');
+      assert.equal(proof.length, 512);
+      assert.equal(tree.verify(leaf[2], 2, root, proof), true);
+      assert.equal(tree.verify(unknownLeaf, 2, root, proof), false);
+      assert.equal(tree.verify(zeroHash, 7, root, tree.proof(7)), true);
+    });
+
+    it('odd leaves', function() {
+      const leaf = Array.from(Array(Math.pow(2, 16)), () => null);
+      leaf[0] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000001', 'hex');
+      leaf[1] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000002', 'hex');
+      leaf[2] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000003', 'hex');
+      leaf[3] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000004', 'hex');
+      leaf[4] = new Buffer('f19587814e8e932897572358b3c0ca6d9cbcc71654b1d312195607aa2b000005', 'hex');
+      const tree = new SparseMerkleTree(16, leaf);
+      const root = tree.root();
+      const proof = tree.proof(1);
+      assert.equal(tree.root().toString('hex'), '010776b71fb8db82fa8d10a2086e69d8b92e95207c78291d1f1aff8355f2d0c6');
+      assert.equal(proof.length, 512);
+      assert.equal(tree.verify(leaf[1], 1, root, proof), true);
+    });
+
+  });
+
+});
