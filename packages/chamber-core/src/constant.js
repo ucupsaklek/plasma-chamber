1ef61f088e2a0833e74c4b724e3388ca66e34445
SHUHEI HIYA <syuhei176@gmail.com>
2019-02-01 20:46:29 +0900

    Remove core and rootchain

--- packages/chamber-core/src/constant.js
+++
@@ -1,6 +0,0 @@
-const BigNumber = require('bignumber.js');
-
-module.exports = {
-  SMT_DEPTH: 16,
-  CHUNK_SIZE: BigNumber('1000000000000000000')
-}
--- packages/chamber-rootchain/README.md
+++
@@ -1,6 +0,0 @@
-plasma-vm-rootchain
-=====
-
-
-Forked from https://github.com/omisego/plasma-mvp
-
